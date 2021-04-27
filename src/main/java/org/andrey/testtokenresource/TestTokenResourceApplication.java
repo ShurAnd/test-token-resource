4c476c77e08b8ab63bd0904e6c7eadafa0ac2a30
AAShurmanov <AAShurmanov@MZ-WD-31000104.mbp>
2021-04-27 15:52:12 +0300

    init commit

---
+++ src/main/java/org/andrey/testtokenresource/TestTokenResourceApplication.java
@@ -0,0 +1,13 @@
+package org.andrey.testtokenresource;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestTokenResourceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestTokenResourceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/andrey/testtokenresource/TestTokenResourceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.andrey.testtokenresource;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestTokenResourceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
